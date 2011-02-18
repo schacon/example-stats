280317336358f9659cf99e016eedfa8c9e5c3851
Scott Chacon <schacon@gmail.com>
2011-02-18 11:44:29 -0800

    lined up output numbers

--- stats.rb
+++ stats.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
 words = File.read('stopwords.txt').split(',')
-puts "Number of words: " + words.size.to_s
+puts "Number of words: ".ljust(25) + words.size.to_s
 
 avg = words.inject(0.0) { |sum, w| sum + w.size.to_f } / words.size
-puts "Average word size: " + avg.to_s
+puts "Average word size: ".ljust(25) + avg.to_s
 
