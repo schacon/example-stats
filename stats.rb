f1919f024657fbb2bfcd461f9489fb4fd3c73515
Scott Chacon <schacon@gmail.com>
2011-02-18 10:58:49 -0800

    add word size average

--- stats.rb
+++ stats.rb
@@ -3,3 +3,6 @@
 words = File.read('stopwords.txt').split(',')
 puts "Number of words: " + words.size.to_s
 
+avg = words.inject(0.0) { |sum, w| sum + w.size.to_f } / words.size
+puts "Average word size: " + avg.to_s
+
