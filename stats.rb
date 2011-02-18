063c8daa0d460f8524aacb341bb0206f0fa1be53
Scott Chacon <schacon@gmail.com>
2011-02-18 11:34:51 -0800

    add count of vowel-started words

--- stats.rb
+++ stats.rb
@@ -6,3 +6,7 @@ puts "Number of words: " + words.size.to_s
 avg = words.inject(0.0) { |sum, w| sum + w.size.to_f } / words.size
 puts "Average word size: " + avg.to_s
 
+vowels = ['a','e','i','o','u']
+vcount = words.select { |word| vowels.include?(word[0,1]) }.size
+puts "Words that start with a vowel: " + vcount.to_s
+
